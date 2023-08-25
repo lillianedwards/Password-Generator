24bc2d778b3be14f70b8004097d95dbbb3c6f196
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-08-25 10:01:47 -0500

    Included more string types in character arrays and wrote README

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Password-Generator
+
+## Description
+
+The employee the password generator was created for needed a program that could randomly generate strong passwords so the senstivie data they have access to is secure. 🔐  
+
+The features of this password generator include:
+ 
+✅ Employee can set the length of the password anyhwere from 8 to 128 characters by way of input.  
+✅ Employee can choose whether to include **special characters** in the password by way of confirmation. #️⃣ 
+✅ Employee can choose whether to include **numeric characters** in the password by way of confirmation. 🔢
+✅ Employee can choose whether to include **lowercase characters** in the password by way of confirmation. 🔡
+✅ Employee can choose whether to include **uppercase characters** in the password by way of confirmation. 🔠
+
+❗️The employee must select one of these types of characters for the password to be generated. 
+
+
+The function used to generate the random password is invoked when the employee clicks the red **Generate Password** button. 🔴
+_______________________________________________________________________
+
+## Access 
+
+To access the random password generator, simply click on the GitHub Pages link in the repo description or follow the link below. 🔗
+[Random Password Generator](lillianedwards.github.io/Password-Generator)
+
+## Installation 
+
+For users new to GitHub, follow the git clone process to copy the code into your local respository. 
+
+## Usage 
+
+![alt text](./assets/images/PasswordGenerator.png)
+
+## Credit
+
+I worked on this project alone, but recived help from my tutor, Andres Jimenez, and my course instructor, Ben Machock.
+
+## License
+This project is not licensed.
--- script.js
+++ script.js
@@ -1,72 +1,82 @@
 //VARIABLE DECLARATIONS
-//add more acceptable characters
+
 var userCharslength;
-var specialChar = ["!", "@", "#", "$"];
-var numericChar = [1,2,3,4,];
-var lowercaseChar = ["a", "b", "c", "d"];
-var uppercaseChar = ["A","B", "C", "D"];
+var specialCharArr = ["!", "@", "#", "$", "%", "&"];
+var numericCharArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var lowercaseCharArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//changed global variable name to idietify arrays so later code isn't shadowed
 
 //this is storing a reference to the button element in the HTML
 var generateBtn = document.querySelector("#generate");
 
+//FUNCTION
 
-//FUNCTIONS
+function generatePassword() {
+  var userCharslength = prompt(
+    "How many characters would you like your password to contain?"
+  );
+  if (userCharslength < 8 || userCharslength > 128) {
+    alert("Please choose a number between 8 and 128.");
+    generatePassword();
+  }
+  var specialChar = confirm(
+    "Click OK to confirm including special characters."
+  );
+  var numericChar = confirm(
+    "Click OK to confirm including numeric characters."
+  );
+  var lowercaseChar = confirm(
+    "Click OK to confirm including lowercase characters."
+  );
+  var uppercaseChar = confirm(
+    "Click OK to confirm including uppercase characters."
+  );
+  if (!specialChar && !numericChar && !lowercaseChar && !uppercaseChar) {
+    alert("Please confirm the use of at least one type of character.");
+    generatePassword();
+  }
+  //gathered users preferences for password
 
-function generatePassword () {
- var userCharslength = prompt("How many characters would you like your password to contain?");
- if (userCharslength <8 || userCharslength> 128){
-  alert("Please choosea number between 8 and 128.")
-  generatePassword();
- };
- var specialChar = confirm("Click OK to confirm including special characters.");
- var numericChar = confirm("Click OK to confirm including numeric characters.");
- var lowercaseChar = confirm("Click OK to confirm including lowercase characters.");
- var uppercaseChar = confirm("Click OK to confirm including uppercase characters.");
-//gathered users preferences for password
+  var userOptions = {
+    length: userCharslength,
+    willHaveSpecialChars: specialChar,
+    willHaveNumericChars: numericChar,
+    willHaveLowerChars: lowercaseChar,
+    willHaveUppChars: uppercaseChar,
+  };
 
-var userOptions = {
-  length: userCharslength,
-  willHaveSpecialChars: specialChar,
-  willHaveNumericChars: numericChar,
-  willHaveLowerChars: lowercaseChar,
-  willHaveUppChars: uppercaseChar,
+  var result = [];
+  var finalPassword = "";
+  if (userOptions.willHaveSpecialChars) {
+    result = result.concat(specialCharArr);
+    //concat is rewriting new array rather than creating an array filled with little arrays
+  }
+  if (userOptions.willHaveNumericChars) {
+    result = result.concat(numericCharArr);
+  }
+  if (userOptions.willHaveLowerChars) {
+    result = result.concat(lowercaseCharArr);
+  }
+  if (userOptions.willHaveUppChars) {
+    result = result.concat(uppercaseCharArr);
+  }
+  for (var i = 0; i < userOptions.length; i++) {
+    var charIndex = Math.floor(Math.random() * result.length);
+    finalPassword = finalPassword + result[charIndex];
+  }
+  return finalPassword;
 };
-
-if (userOptions.willHaveSpecialChars ) {
-  //let's randomly grab a memeber from the specialChar array
-  //push into results array
-}
-if (userOptions.willHaveNumericChars ) {
-  //let's randomly grab a memeber from the numeric characters array
-  //push into results array
-}
-if (userOptions.willHaveLowerChars ) {
-  //let's randomly grab a memeber from the lower characters array
-  //push into results array
-}
-if (userOptions.willHaveUppChars ) {
-  //let's randomly grab a memeber from the upper characters array
-  //push into results array
-}
-var result = [];
-//Eventually this array will store the random generated password. I will use push method to add to this array and this is what I will return to the user
-}
-
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  //Storing a reference to the textarea element 
+  //Storing a reference to the textarea element
 
   passwordText.value = password;
 }
 
 
-
-// Add event listener to generate button
-// event is a click, "whenever the generatebtn is clicked, write this passwork"
 //reference.addEvent.listerner.run this function is taking place
 generateBtn.addEventListener("click", writePassword);
-
